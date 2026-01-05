e1f30cea78debb14b90164ee8901c16640194106
KEBAS Faycel <faycel.kebas@ensia.edu.dz>
2026-01-05 17:25:38 +0100

    final footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <>
-      <footer className="w-full text-lg relative text-center flex justify-center p-5 backdrop-blur-xs bg-black/5 items-center z-50 ">
+      <footer className="w-full text-md md:text-lg relative text-center flex justify-center p-5 backdrop-blur-xs bg-black/5 items-center z-50 ">
         <p className="text-white/70">&copy; Kebas Faycel {new Date().getFullYear()}, All rights reserved </p>
       </footer>
     </>
